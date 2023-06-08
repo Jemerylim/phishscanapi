8a73e9ffc2303d86739291760e68b9daa7f8247b
Jemerylim <limjeremy16@gmail.com>
2023-06-08 14:54:05 +0800

    test

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
-
 app = Flask(__name__)
+
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def get_incomes():
+    return jsonify(incomes)
 
+
+@app.route('/', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
