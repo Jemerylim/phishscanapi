c0bb88d8f1864fb4bf4f7ceed0f451d7138a6e93
Jemerylim <limjeremy16@gmail.com>
2023-07-03 17:06:12 +0800

    form page

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = Flask(__name__,
 def home():
     return render_template('index.html')
 
-@app.route('/form', methods = ['GET'])
+@app.route('/form')
 def form():
     return render_template("email_form.html")
 
